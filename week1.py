d51067d162e618157587a8e83b23aa06180b71e6
Brandon Fazal <fazal@Brandons-Air.fios-router.home>
2022-06-22 22:23:45 -0500

    basic python review:

--- week1.py
+++ week1.py
@@ -1,1 +1,26 @@
-print("hello World")
+## intro
+#  print("hello World")
+
+## input and print
+# name = input("Name: ")
+# print(f"Hello, {name}")
+
+##conditionals
+# n = int(input("Number: "))
+# if n > 0:
+#     print("n is positive")
+# else:
+#     print("n is negative")
+
+##list -- mutable values (can change elements)
+# name = ["Harry", "ron", "her"]
+
+# print(name[0])
+
+##tuple -- immutable values (can't add, have to create new)
+# coordinate = (10.0, 20.0)
+
+##set -- collection of unique values. no set order
+
+##dict-- collection of key-value pairs
+##can look up something and returned a value
