a6f657bbbb20e1ec818881fba55b2ce2dcd50894
Brandon Fazal <fazal@Brandons-Air.fios-router.home>
2022-06-22 22:29:38 -0500

    more basic python

---
+++ lists.py
@@ -0,0 +1,9 @@
+# define a list of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+#exploring built in functions
+names.append("Draco")
+#can modify order
+names.sort()
+
+print(names)
---
+++ sets.py
@@ -0,0 +1,19 @@
+#create empty set
+
+s = set()
+ 
+#add elements
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+
+s.remove(2)
+
+print(s)
+
+print(f"the set has {len(s)} elements.")
+
+#can add, remove from sets. no duplicates
